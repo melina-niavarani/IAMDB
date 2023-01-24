af63f79817030ea281c8f347d3e70a15bd41916e
Melina Niavarani <91658429+melina-niavarani@users.noreply.github.com>
2023-01-24 18:34:16 +0330

    home cover commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: [
+      "./index.html",
+      "./src/**/*.{vue,js,ts,jsx,tsx}",
+    ],
+    theme: {
+      screens: {
+        sm: '360px',
+        md: '550px',
+      },
+      extend: {
+        colors: {
+          'light-gray': '#EEEBDD',
+          'background-color': '#1B1717',
+          'accent-color': '#CE1212',
+          'accent-hover': '#8E0D0D'
+        },
+        fontSize: {
+          xxl:['96px', '113px'],
+          xl: ['48px', {
+            lineHeight: '56px',
+          }],
+          lg: ['21px', '25px'],
+          sm: ['16px', '20px']
+        },
+        
+      },
+    },
+    plugins: [], 
+  }
