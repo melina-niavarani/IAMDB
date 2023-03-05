7da21e6d20a1c6b5194934b9b31bebc71295c1a5
Melina Niavarani <91658429+melina-niavarani@users.noreply.github.com>
2023-03-05 14:51:07 +0330

    add matchItems component to search component

---
+++ src/components/Home/MatchItems.vue
@@ -0,0 +1,21 @@
+<template>
+    <ul>
+        <li v-for="movie in _movie.slice(0, 6)" :key="movie.id">
+            <router-link :to="'/details/' + movie.id" class="flex mb-2 hover:text-accent-color hover:cursor-pointer">
+                <img class="w-[90px] h-[110px]" :src="movie.image"  >
+                <div class="pl-4 inline-block ">
+                    <h3 class="mt-4 text-sm overflow-y-scroll mb-4 md:text-md md:w-[162px] "> {{ movie.title }}</h3>
+                    <p class="opacity-50 text-xs md:text-sm"> {{ movie.description }} </p>
+                </div>
+            </router-link>
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+    props: [
+        "_movie"
+    ]
+}
+</script>
