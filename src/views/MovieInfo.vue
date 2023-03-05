ae67647628c9b8fefa3b6d465932db4e6ac1cdab
Melina Niavarani <91658429+melina-niavarani@users.noreply.github.com>
2023-03-05 14:52:16 +0330

    fetch movie poster

--- src/views/MovieInfo.vue
+++ src/views/MovieInfo.vue
@@ -3,7 +3,7 @@
          :style="{ backgroundImage: `linear-gradient(rgba(27, 23, 23, 0) 58.33%,
                                     rgba(27, 23, 23, 0.8) 71.23%,
                                     #1B1717 100%),
-                                    url(${ movies.image})` }">
+                                    url(${ poster })` }">
         <!-- Movie Details -->
         <header class="px-[15px] md:px-[132px] pt-[164px] md:flex md:pr-[143px] md:pt-[521px] md:space-x-[60px]">
                 <Cover :_movie="movies"/>
@@ -12,11 +12,11 @@
         </header>
         <main>
             <!-- Photos -->
-            <div class="px-[15px] md:px-[132px]"><Photos :_images="images"/></div>
+            <Photos class="px-[15px] md:px-[132px]" :_images="images"/>
             <!-- More Like This -->
             <MoreLikeThis :_movie="movies"/>
             <!-- FAQs -->
-            <div class="px-[15px] md:px-[132px]"><Quastions :_movie="movies"/></div>
+            <Quastions class="px-[15px] md:px-[132px]" :_movie="movies"/>
         </main>
     </div>
 </template>
@@ -42,36 +42,46 @@
         },
         data(){
             return {
+                apiKey1: 'k_61gu5fbz',
+                apiKey2: 'k_crzfp1ws',
                 movies: '',
                 images: '',
+                poster: '',
                 route : useRoute()
             }
         },
         methods: { 
             getInfo(){
                 if ( this.searchContent != ''){
-                    // fetch(`http://www.omdbapi.com/?apikey=29f7e005&i=${this.route.params.id}`)
-                    fetch(`https://imdb-api.com/en/API/Title/k_61gu5fbz/${this.route.params.id}`, this.requestOptions)
+                    fetch(`https://imdb-api.com/en/API/Title/${this.apiKey2}/${this.route.params.id}`, this.requestOptions)
                     .then(response => response.json())
                     .then(result => {
                         this.movies = result
-                        // this.movies = result.results;
                     })
                     .catch(error => console.log('error', error));
                 }
             },
-            getPosters(){
-                fetch(`https://imdb-api.com/API/Images/k_61gu5fbz/${this.route.params.id}`, this.requestOptions)
+            getImages(){
+                fetch(`https://imdb-api.com/API/Images/${this.apiKey2}/${this.route.params.id}`, this.requestOptions)
                     .then(response => response.json())
                     .then(result => {
                         this.images = result
+                    })
+                    .catch(error => console.log('error', error));
+            },
+            getPoster(){
+                fetch(`https://imdb-api.com/en/API/Posters/${this.apiKey2}/${this.route.params.id}`, this.requestOptions)
+                    .then(response => response.json())
+                    .then(result => {
+                        this.poster = result.posters[1].link
                     })
                     .catch(error => console.log('error', error));
             }
         },
         created() {
             this.getInfo()
-            this.getPosters()
+            this.getImages()
+            this.getPoster()
         }
     }
 </script>
