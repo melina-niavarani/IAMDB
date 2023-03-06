b348659c84346c045814f5d64961b7265b617676
Melina Niavarani <91658429+melina-niavarani@users.noreply.github.com>
2023-03-06 20:19:04 +0330

    install carousel npm and add it to MoreLikeThis

--- src/components/main/more/MoreLikeList.vue
+++ src/components/main/more/MoreLikeList.vue
@@ -1,53 +1,48 @@
 <template>
-    <section duration="180s" delay="5s" 
-    v-on:mouseover="paused=true">
-        <ul class="flex flex-nowrap overflow-x-scroll gap-6" >
-            <li v-for="likeThis in _movie.similars">
-                <router-link :to="'/details/' + likeThis.id" class="hover:cursor-pointer">
-                    <div><img class="rounded-xl min-w-[230px] w-[342px] h-[342px] md:w-[409px] md:min-w-[276px] md:h-[409px]" :src="likeThis.image" alt=""></div>
-                    <div class="flex justify-between mt-[13px] md:mt-3">
-                        <h5 class="text-sm font-bold md:text-lg">{{ likeThis.title }}</h5>
-                        <div>
-                            <span v-if="isFavourite" class="Favourite bg-red-heart"></span>
-                            <span class="star bg-star"></span>
-                            <span class="text-sm opacity-40 md:text-md"> {{ likeThis.imDbRating }}</span>
+    <section >
+       
+        <carousel  :itemsToShow="3.95" :transition="10" :wrapAround="true" :autoplay="4000" >
+            <slide v-for="likeThis in _movie.similars" :key="_movie.id" >
+                <div class="mx-3">
+                    <router-link :to="'/details/' + likeThis.id" class="hover:cursor-pointer ">
+                        <div><img class="rounded-xl min-w-[230px] w-[342px] h-[342px] md:w-[409px] md:min-w-[276px] md:h-[409px]" :src="likeThis.image" alt=""></div>
+                        <div class="flex justify-between mt-[13px] md:mt-3">
+                            <h5 class="text-sm font-bold md:text-lg">{{ likeThis.title }}</h5>
+                            <div>
+                                <span v-if="isFavourite" class="bg-red-heart inline-block bg-no-repeat w-4 h-4 mt-[3px] mx-1.5"></span>
+                                <span class="bg-star inline-block bg-no-repeat w-4 h-4 mt-[3px] mx-1.5"></span>
+                                <span class="text-sm opacity-40 md:text-md"> {{ likeThis.imDbRating }}</span>
+                            </div>
                         </div>
-                    </div>
-                </router-link>
-            </li>
-        </ul>
+                     </router-link>
+                </div>
+            </slide>
+            ...
+        </carousel>
+      
     </section>
 </template>
 
 <script>
-import InfiniteSlideBar from 'vue3-infinite-slide-bar';
+import 'vue3-carousel/dist/carousel.css'
+import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel'
 
 export default {
     components: {
-        InfiniteSlideBar
+        Carousel,
+        Slide
     },
     data() {
         return {
-            isFavourite: false
+            isFavourite: true,
         }
     },
     props: [
         "_movie" ,
     ],
-    computed: {
-        
+    methods: {
+
     }
 }
 </script>
 
-<style scoped>
-    .star,
-    .Favourite {
-        display: inline-block;
-        background-repeat: no-repeat;
-        width: 16px;
-        height: 16px;
-        margin-top: 3px;
-        margin-inline: 6px;
-    }
-</style>
