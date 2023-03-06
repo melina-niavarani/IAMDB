42040b26b47b601dd893f0c083e87d49c5b84c97
Melina Niavarani <91658429+melina-niavarani@users.noreply.github.com>
2023-03-06 10:45:34 +0330

    add object position to photos

--- src/components/main/photos/PhotosList.vue
+++ src/components/main/photos/PhotosList.vue
@@ -9,7 +9,7 @@
         <ul class="flex flex-wrap gap-3 md:gap-6">
             <li class="photo-container relative rounded-xl  bg-black hover:cursor-pointer "
             v-for = "image in _images.items.slice(0, 10) " :key="image.title" >
-                <img class="rounded-xl h-[102.4] w-[159px] md:h-[144px] md:w-[216px] object-cover"
+                <img class="rounded-xl h-[102.4] w-[159px] md:h-[144px] md:w-[216px] object-cover object-top"
                  :src="image.image" :alt="image.title"
                  @click="enlargeImg(image.image)"  >
                 <div class="absolute expand opacity-0"></div>
