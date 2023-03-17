02735bd2bf9f25441346cc65ee36c4adc3f2478a
Melina Niavarani <91658429+melina-niavarani@users.noreply.github.com>
2023-03-18 01:35:27 +0330

    add hmrupdate to pinia

--- src/stores/store.js
+++ src/stores/store.js
@@ -1,4 +1,4 @@
-import { defineStore } from "pinia";
+import { defineStore, acceptHMRUpdate } from "pinia";
 import { useRoute } from "vue-router";
 
 export const useStorageList = defineStore('storeFavoriteMovies', {
@@ -22,16 +22,10 @@ export const useStorageList = defineStore('storeFavoriteMovies', {
 export const useFetchData = defineStore('fetchMovieData', {
     state: () => {
         return {
-            // base_url: 'https://imdb-api.com/en/API',
-            // title_url: base_url + '/Title',
-            // images_url: base_url + '/Images',
-            // posters_url: base_url + '/Posters',
-            // faqs_url: base_url + '/FAQ',
-            // trailers_url: base_url + '/Trailer',
-            apiKey1: 'k_61gu5fbz',
-            apiKey2: 'k_crzfp1ws',
-            apiKey3: 'k_f2h9lgy2',
-            apiKey4: 'k_35gf5vid',
+            base_url: 'https://imdb-api.com/API',
+            // apiKey: 'k_5y2377sf',
+            // apiKey: 'k_crzfp1ws',
+            apiKey: 'k_f2h9lgy2',
             route : useRoute(),
             movies: '',
             images: '',
@@ -45,36 +39,47 @@ export const useFetchData = defineStore('fetchMovieData', {
             return Math.floor(Math.random() * (max - min) + min)
         },
         async getInfo(){
-            await fetch(`https://imdb-api.com/API/Title/${this.apiKey4}/${this.route.params.id}`)
+            await fetch(`${this.base_url}/Title/${this.apiKey}/${this.route.params.id}`)
             .then(response => response.json())
             .then(result => {
                 this.movies = result
             })
-            .catch(error => console.log('error', error));
-            await fetch(`https://imdb-api.com/API/Trailer/${this.apiKey4}/${this.route.params.id}`)
-            .then(response => response.json())
-            .then(result => {
-                this.movieTrailer = result.linkEmbed
-            })
-            .catch(error => console.log('error', error));
-            await fetch(`https://imdb-api.com/API/FAQ/${this.apiKey4}/${this.route.params.id}`)
-            .then(response => response.json())
-            .then(result => {
-                this.faq = result
-            })
-            .catch(error => console.log('error', error));
-            await fetch(`https://imdb-api.com/API/Images/${this.apiKey4}/${this.route.params.id}`)
-            .then(response => response.json())
-            .then(result => {
-                this.images = result.items.slice(0, 10)
-            })
-            .catch(error => console.log('error', error));
-            await fetch(`https://imdb-api.com/en/API/Posters/${this.apiKey4}/${this.route.params.id}`)
-            .then(response => response.json())
-            .then(result => {
-                this.poster = result.posters[this.getRandom(0, result.posters.length)].link
-            })
-            .catch(error => console.log('error', error));
+            .catch(error => 
+                // this.route.replace('/:pathMatch(.*)*'),
+                console.log('error', error))
+            setTimeout(() => {
+                fetch(`${this.base_url}/Trailer/${this.apiKey}/${this.route.params.id}`)
+                .then(response => response.json())
+                .then(result => {
+                    this.movieTrailer = result.linkEmbed
+                })
+                .catch(error => console.log('error', error));
+
+                fetch(`${this.base_url}/FAQ/${this.apiKey}/${this.route.params.id}`)
+                .then(response => response.json())
+                .then(result => {
+                    this.faq = result
+                })
+                .catch(error => console.log('error', error));
+
+                fetch(`${this.base_url}/Images/${this.apiKey}/${this.route.params.id}`)
+                .then(response => response.json())
+                .then(result => {
+                    this.images = result.items.slice(0, 10)
+                })
+                .catch(error => console.log('error', error));
+                
+                fetch(`${this.base_url}/Posters/${this.apiKey}/${this.route.params.id}`)
+                .then(response => response.json())
+                .then(result => {
+                    this.poster = result.posters[this.getRandom(0, result.posters.length)].link
+                })
+                .catch(error => console.log('error', error));
+            }, "5000")
         },
     },
-}) 
+});
+
+if (import.meta.hot) {
+    import.meta.hot.accept(acceptHMRUpdate(useFetchData, import.meta.hot))
+}
